8b67e48185e5e7affa005303afede7f207b7f49b
Uri Shaked <uri@urishaked.com>
2021-03-24 14:58:10 +0200

    feat: add NDJSON support + ability to specify FPS

--- README.md
+++ README.md
@@ -1,26 +1,107 @@
 # LED Animation Recorder
 
-Use this tool to record Wokwi LED animations into a binary file.
+Use this tool to export LED animation from Wokwi into [NDJSON](http://ndjson.org/) / [JSON Lines](https://jsonlines.org/) or binary files.
 
-## File format
+## Usage
+
+Add the following part to your [diagram.json file](https://docs.wokwi.com/diagram-format), specifying the number of pixels in the strip/matrix:
+
+```json
+{
+  "type": "wokwi-neopixel-strip",
+  "id": "neopixels",
+  "attrs": { "pixels": "3" }
+}
+```
+
+Note that the part id _must_ be `neopixels` for the recorder to work.
+
+and then add a customView:
+
+```json
+  "customView": "https://wokwi.github.io/led-recorder",
+```
+
+Example for a complete diagram.json file:
+
+```json
+{
+  "version": 1,
+  "author": "Uri Shaked",
+  "editor": "wokwi",
+  "customView": "https://wokwi.github.io/led-recorder",
+  "parts": [
+    {
+      "type": "wokwi-arduino-uno",
+      "id": "uno",
+      "top": 0,
+      "left": 150
+    },
+    {
+      "type": "wokwi-neopixel-strip",
+      "id": "neopixels",
+      "attrs": { "pixels": "3" }
+    }
+  ],
+  "connections": [["uno:3", "neopixels:DIN", ""]]
+}
+```
+
+## File format (NDJSON / JSON Lines)
+
+Each line is a JSON object. The first line is a header line, which contains information about the recording. The animation frames are encoded in the subsequent lines, one line per frame.
+
+The header contains the following fields:
+
+| Name      | Type   | Description                          |
+| --------- | ------ | ------------------------------------ |
+| magic     | string | "WokwiLED"                           |
+| version   | number | Always 1                             |
+| pixels    | number | Number of pixels per frame           |
+| frameRate | number | Frames per second (usually 30 or 60) |
+
+The frames contain the following fields:
+
+| Name      | Type   | Description                             |
+| --------- | ------ | --------------------------------------- |
+| frame     | number | Frame index, starting from 0            |
+| leds      | array  | LED values:                             |
+| leds[n].r | number | The red value of pixel `n`              |
+| leds[n].g | number | The green value of pixel `n`            |
+| leds[n].b | number | The blue value of pixel `n`             |
+| leds[n].a | number | The alpha value of pixel `n` (optional) |
+
+Pixel values (r, g, b, a) are integer values between 0 and 255.
+
+Example:
+
+```json
+{ "magic": "WokwiLED", "version": 1, "pixels": 3, "frameRate": 30 }
+{ "frame": 0, "leds": [ {"r":255,"g":0,"b":0}, {"r":0,"g":0,"b":0}, {"r":0,"g":0,"b":0} ] }
+{ "frame": 1, "leds": [ {"r":0,"g":0,"b":0}, {"r":0,"g":255,"b":0}, {"r":0,"g":0,"b":0} ] }
+{ "frame": 2, "leds": [ {"r":0,"g":0,"b":0}, {"r":0,"g":0,"b":0}, {"r":0,"g":0,"b":255} ] }
+{ "frame": 3, "leds": [ {"r":0,"g":0,"b":0}, {"r":0,"g":255,"b":0}, {"r":0,"g":0,"b":0} ] }
+...
+```
+
+## File format (binary)
 
 The file begins with a 32 byte header, followed by animation frames. All values are little endian.
 
 The header contains the following fields:
 
-| Offset | Bytes | Name    | Description                |
-| ------ | ----- | ------- | -------------------------- |
-| 0      | 8     | magic   | "WokwiLED"                 |
-| 8      | 4     | version | Always 1                   |
-| 12     | 4     | nframes | Number of frames           |
-| 16     | 4     | npixels | Number of pixels per frame |
-| 20     | 12    |         | Reserved, set to 0         |
+| Offset | Bytes | Name    | Description                 |
+| ------ | ----- | ------- | --------------------------- |
+| 0      | 8     | magic   | "WokwiLED"                  |
+| 8      | 4     | version | Always 1                    |
+| 12     | 4     | nframes | Number of frames            |
+| 16     | 4     | npixels | Number of pixels per frame  |
+| 20     | 4     | fps     | Number of frames per second |
+| 24     | 8     |         | Reserved, set to 0          |
 
 Each frame is an array of `npixels` DWORDs. Each element is in the following
 format: `0x00GGRRBB`, where `GG` indicatates the green value, `RR` indicates
 the red value, and `BB` (the lowest byte) indicates the blue value.
-
-The player currently assumes 60fps.
 
 ## License
 
--- led-recorder.js
+++ led-recorder.js
@@ -6,42 +6,137 @@
  * Released under the MIT license.
  */
 
-const recording = [];
 const statusEl = document.getElementById('status');
-statusEl.text = 'Ready to capture';
+const fileTypeEl = document.getElementById('file-type');
+const fpsEl = document.getElementById('fps');
 
-function recordEvent(pixels) {
-  statusEl.textContent = `Captured ${recording.length} frames`;
-  recording.push(pixels);
+function updateStatus(status) {
+  statusEl.textContent = status;
 }
 
-function download() {
-  if (!recording.length) {
-    alert('no data recorded!');
-    return;
+function getFPS() {
+  return parseInt(fpsEl.value, 10);
+}
+
+class Recorder {
+  constructor() {
+    this.reset();
   }
 
+  recordEvent(nanos, pixels) {
+    this.recording.push({ nanos, pixels });
+  }
+
+  get frameSize() {
+    return this.recording[0].pixels.length;
+  }
+
+  frameCount(fps) {
+    const { recording } = this;
+    if (!recording.length) {
+      return 0;
+    }
+    const startNanos = recording[0].nanos;
+    const lastNanos = recording[recording.length - 1].nanos;
+    const frameNanos = Math.round(1e9 / fps);
+    return Math.ceil((lastNanos - startNanos + 1) / frameNanos);
+  }
+
+  *getFrames(fps) {
+    const numFrames = this.frameCount(fps);
+    const frameNanos = Math.round(1e9 / fps);
+    const { recording } = this;
+    let recordingIndex = 0;
+    let timestamp = recording[0].nanos;
+    for (let frame = 0; frame < numFrames; frame++) {
+      yield { frame, pixels: recording[recordingIndex].pixels };
+      timestamp += frameNanos;
+      while (
+        recordingIndex + 1 < recording.length &&
+        recording[recordingIndex + 1].nanos <= timestamp
+      ) {
+        recordingIndex++;
+      }
+    }
+  }
+
+  reset() {
+    this.recording = [];
+  }
+}
+
+const recorder = new Recorder();
+
+function downloadBlob(fileName, blob) {
   const a = document.createElement('a');
   document.body.appendChild(a);
   a.style = 'display: none';
-  const frameCount = recording.length;
-  const frameSize = recording[0].length;
+  const url = window.URL.createObjectURL(blob);
+  a.href = url;
+  a.download = fileName;
+  a.click();
+  window.URL.revokeObjectURL(url);
+  document.body.removeChild(a);
+}
+
+function exportNDJSON() {
+  const fps = getFPS();
+  const header = JSON.stringify({
+    magic: 'WokwiLED',
+    version: 1,
+    pixels: recorder.frameSize,
+    frameRate: fps,
+  });
+  const lines = [header];
+  for (const { frame, pixels } of recorder.getFrames(fps)) {
+    const leds = Array.from(pixels).map((item) => ({
+      g: (item >> 16) & 0xff,
+      r: (item >> 8) & 0xff,
+      b: item & 0xff,
+    }));
+    lines.push(JSON.stringify({ frame, leds }));
+  }
+  const blob = new Blob([lines.join('\n')], { type: 'application/x-ndjson' });
+  downloadBlob('animation.ndjson', blob);
+}
+
+function exportBinary() {
+  const fps = getFPS();
+  const frameCount = recorder.frameCount(fps);
+  const frameSize = recorder.frameSize;
   const data = new Uint32Array(frameCount * frameSize + 8);
   data[0] = 0x776b6f57; // magic 1
   data[1] = 0x44454c69; // magic 2
   data[2] = 1; // version
   data[3] = frameCount;
   data[4] = frameSize;
-  for (let i = 0; i < frameCount; i++) {
-    data.set(recording[i], 8 + i * frameSize);
+  data[5] = fps;
+  for (const { frame, pixels } of recorder.getFrames(fps)) {
+    data.set(pixels, 8 + frame * frameSize);
   }
   const blob = new Blob([data], { type: 'octet/stream' });
-  const url = window.URL.createObjectURL(blob);
-  a.href = url;
-  a.download = 'animation.led';
-  a.click();
-  window.URL.revokeObjectURL(url);
-  document.body.remove(a);
+  downloadBlob('animation.led', blob);
+}
+
+function download() {
+  if (!recorder.frameCount(getFPS())) {
+    alert('Error: no data captured!');
+    return;
+  }
+
+  switch (fileTypeEl.value) {
+    case 'ndjson':
+      return exportNDJSON();
+    case 'binary':
+      return exportBinary();
+    default:
+      alert(`Unsupported file type: ${fileTypeEl.value}`);
+  }
+}
+
+function reset() {
+  recorder.reset();
+  updateStatus('Ready to capture');
 }
 
 // Workaround for a Wokwi sometimes missing the first message
@@ -51,10 +146,13 @@ let listener = setInterval(() => {
 
 window.addEventListener('message', ({ data }) => {
   if (data.neopixels) {
-    recordEvent(data.neopixels);
+    recorder.recordEvent(data.$nanos, data.neopixels);
+    updateStatus(`Captured ${recorder.frameCount(getFPS())} frames`);
     if (listener) {
       clearInterval(listener);
       listener = null;
     }
   }
 });
+
+updateStatus('Ready to capture');
